b5c84fce1399dbe501033317f2523781d225de38
Casey Deccio <casey@deccio.net>
2022-01-18 15:46:40 -0700

    Add handler for SIGINT

--- hw-signals/killer.c
+++ hw-signals/killer.c
@@ -6,9 +6,25 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void sigint_handler(int signum) {
+	// send SIGKILL to all processes in group, so this process and children
+	// will terminate.  Use SIGKILL because SIGTERM and SIGINT (among
+	// others) are overridden in the child.
+	kill(-getpgid(0), SIGKILL);
+}
+
 int main(int argc, char *argv[]) {
 	char *scenario = argv[1];
 	int pid = atoi(argv[2]);
+
+	struct sigaction sigact;
+
+	// Explicitly set flags
+	sigact.sa_flags = SA_RESTART;
+	sigact.sa_handler = sigint_handler;
+	// Override SIGINT, so that interrupting this process sends SIGKILL to
+	// this one and, more importantly, to the child.
+	sigaction(SIGINT, &sigact, NULL);
 
 	switch (scenario[0]) {
 	case '0':
